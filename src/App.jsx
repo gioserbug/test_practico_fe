022490dc3168a75433664c601810aec04bf9bcaf
Sergio Cano <gioserbug@gmail.com>
2024-04-08 10:28:26 -0500

    fix: improve performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./views/Layout";
-import List from "./views/List";
-import Detail from "./views/Detail";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "sonner";
-import "react-loading-skeleton/dist/skeleton.css";
-import "./styles/App.scss";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './views/Layout';
+import List from './views/List';
+import Detail from './views/Detail';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'sonner';
+import 'react-loading-skeleton/dist/skeleton.css';
+import './styles/App.scss';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: { queries: { refetchOnWindowFocus: false } },
-  });
-
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster richColors position="top-right" />
+      <Toaster richColors position='top-right' />
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/items" element={<List />} />
-          <Route path="/items/:id" element={<Detail />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='/items' element={<List />} />
+          <Route path='/items/:id' element={<Detail />} />
+          <Route path='*' element={<Navigate to='/' replace />} />
         </Route>
       </Routes>
     </QueryClientProvider>
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,42 +1,40 @@
-import { useLocation } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { getItems } from "../services/items.services";
-import Skeleton from "react-loading-skeleton";
-import Card from "../components/Card";
-import { BreadcrumbList } from "../components/Breadcrumb";
-import { useEffect } from "react";
+import { useLocation } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getItems } from '../services/items.services';
+import Skeleton from 'react-loading-skeleton';
+import Card from '../components/Card';
+import { BreadcrumbList } from '../components/Breadcrumb';
+import { useEffect } from 'react';
 
 const List = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const search = searchParams.get("search");
-
-  if (!search) {
-    return null;
-  }
+  const search = searchParams.get('search');
 
   const { data, isFetching } = useQuery({
-    queryKey: ["items", search],
+    queryKey: ['items', search],
     queryFn: () => getItems(search),
   });
 
   useEffect(() => {
-    localStorage.setItem("search", search);
-    localStorage.setItem("categoryItem", data?.categories?.[0].name);
+    localStorage.setItem('search', search);
+    localStorage.setItem('categoryItem', data?.categories?.[0].name);
   }, [search, data?.categories]);
+
+  if (!search) return null;
 
   return (
     <>
       <BreadcrumbList categories={data?.categories} />
-      <div className="list">
+      <div className='list'>
         {isFetching ? (
-          <Skeleton height={245} count={4} style={{ marginBottom: "5px" }} />
+          <Skeleton height={245} count={4} style={{ marginBottom: '5px' }} />
         ) : data?.items?.length > 0 ? (
           data.items
             ?.slice(0, 4)
             .map((item, index) => <Card key={index} item={item} />)
         ) : (
-          <h4 style={{ textAlign: "center" }}>
+          <h4 style={{ textAlign: 'center' }}>
             No hay publicaciones que coincidan con tu búsqueda.
           </h4>
         )}
